df7b78c1e4c4487eca4b65a3f5fb87d9d4598e18
Lei <ylxx@live.com>
2022-01-27 18:52:37 +0800

    try py3, but fail

--- flask/socket.io.py
+++ flask/socket.io.py
@@ -12,28 +12,32 @@ if is_pi:
     import max7219.led as led
     import time
     from max7219.font import proportional, SINCLAIR_FONT, TINY_FONT, CP437_FONT
-    rc = lambda x:int(x*random.random())
+
+    rc = lambda x: int(x * random.random())
     device = led.matrix(cascaded=1)
     device.orientation(180)
     device.brightness(15)
+
     def flush_device(buf):
-        
+
         w = len(buf[0])
         h = len(buf)
         device.clear()
         for y in range(h):
             for x in range(w):
                 if buf[x][y] != 0:
-                    device.pixel(x,y,1)
-                
+                    device.pixel(x, y, 1)
+
     def code_buffer(code):
-        device._buffer=code
+        device._buffer = code
         device.flush()
+
+
 else:
     import webbrowser
-    
 
-from flask import Flask, abort,render_template,redirect,url_for,g
+
+from flask import Flask, abort, render_template, redirect, url_for, g
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -42,121 +46,114 @@ app.debug = False
 socketio = SocketIO(app)
 
 import os
-print os.path.abspath('.')
-path = r'C:/Users/yl/Desktop/py/python/raspberry/RetroPython/'
-path = '../'
-if is_pi==1:
-    path = './'
+
+print(os.path.abspath("."))
+path = r"C:/Users/yl/Desktop/py/python/raspberry/RetroPython/"
+path = "../"
+if is_pi == 1:
+    path = "./"
 
 
-players = [0,0]
+players = [0, 0]
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return '<h1>Url wrong! 404 not found</h1>', 404
-    
+    return "<h1>Url wrong! 404 not found</h1>", 404
+
+
 @app.errorhandler(403)
 def page_not_found(e):
-    return '<h1>Already has two plyer,please wite for a while Ture</h1>', 403
+    return "<h1>Already has two plyer,please wite for a while Ture</h1>", 403
 
 
-
-@socketio.on('disconnect', namespace='/game')
+@socketio.on("disconnect", namespace="/game")
 def test_disconnect():
-    print('Client disconnected')
-    
-    
-@app.route('/canvas')
+    print("Client disconnected")
+
+
+@app.route("/canvas")
 def show_canvas():
-    h = 'RetroPython.html'
+    h = "RetroPython.html"
     f = open(path + h)
     html = f.read()
     f.close()
     return html
 
-@app.route('/0')
+
+@app.route("/0")
 def get_0_page():
 
-    return redirect('/canvas')
+    return redirect("/canvas")
 
 
-    
-@app.route('/')
+@app.route("/")
 def get_index_page():
 
-    return redirect('/2')
+    return redirect("/2")
 
 
-
-@app.route('/static/<filee>')
+@app.route("/static/<filee>")
 def get_static(filee):
-    f = open(path + 'static/' + filee)
+    f = open(path + "static/" + filee)
     html = f.read()
     f.close()
 
     return html
 
-@app.route('/2')
+
+@app.route("/2")
 def get_p2():
 
-    f = open(path+'p2.html')
+    f = open(path + "p2.html")
     html = f.read()
     f.close()
     return html
-    
-@app.route('/1')
+
+
+@app.route("/1")
 def get_p1():
 
-    f = open(path+'p1.html')
+    f = open(path + "p1.html")
     html = f.read()
     f.close()
     return html
-    
-@socketio.on('testLine', namespace='/game')
+
+
+@socketio.on("testLine", namespace="/game")
 def test_connect(message):
-    print message['data']
-    emit('testLine', {'data': 'successed to phone!', 'count': 0})
-     
+    print(message["data"])
+    emit("testLine", {"data": "successed to phone!", "count": 0})
 
-@socketio.on('p2_to_p1', namespace='/game')
+
+@socketio.on("p2_to_p1", namespace="/game")
 def p2_to_p1(p2):
-    emit('p2_to_p1', p2, broadcast=True)
+    emit("p2_to_p1", p2, broadcast=True)
 
-@socketio.on('p1_to_pc', namespace='/game')
+
+@socketio.on("p1_to_pc", namespace="/game")
 def p2_to_p1(p1):
-    emit('p1_to_pc', p1, broadcast=True)
-    
-    
-@socketio.on('end', namespace='/game')
-def end(result): # game over
-    emit('end', result, broadcast=True)
-    
-    message = "p"+str(result)+" win! "
+    emit("p1_to_pc", p1, broadcast=True)
+
+
+@socketio.on("end", namespace="/game")
+def end(result):  # game over
+    emit("end", result, broadcast=True)
+
+    message = "p" + str(result) + " win! "
     if is_pi == 0:
         return
     if result == 0:
-        message == 'Die together!,no winer! '    
+        message == "Die together!,no winer! "
     for i in range(2):
         device.show_message(message, font=SINCLAIR_FONT)
-        
-@socketio.on('buffer', namespace='/game')
+
+
+@socketio.on("buffer", namespace="/game")
 def led_buffer(buff):
     code_buffer(buff)
-    
-    
+
+
 if __name__ == "__main__":
 
-    socketio.run(app, host='0.0.0.0', port=80)
-
-    
-    
-
-
-
-
-
-
-
-
-
-
+    socketio.run(app, host="0.0.0.0", port=80)
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# RetroPython 简要配置说明 v0.10
+# RetroPython 简要配置说明 v0.11
 ----
 > 作者：`小磊`
 
@@ -16,34 +16,39 @@
 
 ### 1. 电脑
 
-Python版本：`Python 2.7`
+Python版本：`Python 2.7` (暂时不支持 Python 3)
 
-Python库：`flask`  `flask_socketio`(安装：`pip install flask flask_socketio` )
+Python库：`flask`  `flask_socketio`(安装：`sudo pip2 install flask flask_socketio` )
 
-浏览器：推荐`Chrome`
+浏览器：推荐`firefox`
 
 ### 2. 手机端
 
-手机版 Chrome：**一定要最新版！**
+手机版 firefox：**一定要最新版！**
 
 
 ## 二. 使用方法：
 
-`cd ./flask`
+```bash
+git clone https://github.com/DIYer22/RetroPython.git
+sudo pip2 install flask flask_socketio
+cd ./flask
+sudo python2 socket.io.py & # (注意:默认是80端口，`Linux`下请使用`sudo`)
 
-`python socket.io.py` (注意:默认是80端口，`Linux`下请使用`sudo`)
-
+sleep 2
+firefox 127.0.0.1/0
+```
 使两台作为控制器的手机与电脑处于同一局域网
 
-电脑的Chrome打开`[电脑局域网IP]/canvas`  例如：`192.168.1.101/canvas`
+电脑的firefox打开`[电脑局域网IP]/canvas`  例如：`192.168.1.101/canvas`
 
 便可见到两条贪吃蛇 ，如图
 
 ![](./static/imgs/retroPython.png)
 
-玩家一用手机Chrome打开`[电脑局域网IP]/1`
+玩家一用手机firefox打开`[电脑局域网IP]/1`
 
-玩家二用手机Chrome打开`[电脑局域网IP]/2`
+玩家二用手机firefox打开`[电脑局域网IP]/2`
 
 玩家手机屏幕应如图显示(建议关闭自适应屏幕)
 
@@ -53,7 +58,7 @@ Python库：`flask`  `flask_socketio`(安装：`pip install flask flask_socketio
 
 ## 三. 注意事项
 
-1. 手机版 Chrome **一定要最新版！**
+1. 手机版 firefox **一定要最新版！**
 2. 由于使用了相对路径,一定要在`./flask`下运行Python,否则找不到文件
 3. 注意80端口是否被占用，或是否有权限使用
 4. 建议关闭手机自适应屏幕
